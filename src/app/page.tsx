7652f9528ba094c7c866f0679e234b7fcc542cca
rafalbagrowski <rbagrowsk@gmail.com>
2024-06-12 17:24:50 -0600

    testing forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ function Newsletter() {
       name="newsletter"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
         // @ts-ignore
-      netlify
+      data-netlify="true"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
@@ -315,6 +315,26 @@ export default async function Home() {
               <Resume/>
             </div>
           </div>
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name"/></label>
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email"/></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </Container>
       </>
   )
