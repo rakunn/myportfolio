4cb2c3025b06bfbc9ff3f67312898885e8a8943b
rafalbagrowski <rbagrowsk@gmail.com>
2024-06-12 18:25:16 -0600

    testing forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import {Button} from "@/components/Button";
+import React from "react";
+import {MailIcon} from "@/components/MailIcon";
+
 export const Form = () => {
     const handleSubmit = (event: any) => {
         event.preventDefault();
@@ -17,20 +21,27 @@ export const Form = () => {
     };
 
     return (
-        <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit} action="/">
-            <input type="hidden" name="form-name" value="contact"/>
-            <p>
-                <label>Your Name: <input type="text" name="name"/></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email"/></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
+        <form name="newsletter" className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40" method="POST" data-netlify="true" onSubmit={handleSubmit} action="/">
+                <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+                    <MailIcon className="h-6 w-6 flex-none"/>
+                    <span className="ml-3">Stay up to date</span>
+                </h2>
+                <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                    Get notified when I publish something new, and unsubscribe at any time.
+                </p>
+                <div className="mt-6 flex">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                        aria-label="Email address"
+                        required
+                        className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+                    />
+                    <Button type="submit" className="ml-4 flex-none">
+                        Join
+                    </Button>
+                </div>
         </form>
     );
 }
