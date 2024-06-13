69d43f5fd6ef66df95afdd2abcb5abf19f5c1a4f
rafalbagrowski <rbagrowsk@gmail.com>
2024-06-12 18:28:14 -0600

    testing forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -21,27 +21,29 @@ export const Form = () => {
     };
 
     return (
-        <form name="newsletter" className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40" method="POST" data-netlify="true" onSubmit={handleSubmit} action="/">
-                <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-                    <MailIcon className="h-6 w-6 flex-none"/>
-                    <span className="ml-3">Stay up to date</span>
-                </h2>
-                <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                    Get notified when I publish something new, and unsubscribe at any time.
-                </p>
-                <div className="mt-6 flex">
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email address"
-                        aria-label="Email address"
-                        required
-                        className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
-                    />
-                    <Button type="submit" className="ml-4 flex-none">
-                        Join
-                    </Button>
-                </div>
+        <form name="newsletter" className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40" method="POST"
+              data-netlify="true" onSubmit={handleSubmit} action="/">
+            <input type="hidden" name="form-name" value="newsletter"/>
+            <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+                <MailIcon className="h-6 w-6 flex-none"/>
+                <span className="ml-3">Stay up to date</span>
+            </h2>
+            <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                Get notified when I publish something new, and unsubscribe at any time.
+            </p>
+            <div className="mt-6 flex">
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email address"
+                    aria-label="Email address"
+                    required
+                    className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+                />
+                <Button type="submit" className="ml-4 flex-none">
+                    Join
+                </Button>
+            </div>
         </form>
     );
 }
